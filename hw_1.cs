695bae4cb897adc745436151d7824f3c45c79455
agafapudova <153856865+agafapudova@users.noreply.github.com>
2024-02-08 18:34:55 +0300

    Homework_final

---
+++ hw_1.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет 
+//все натуральные числа в промежутке от M до N. 
+//рекурсию, не использовать циклы.
+
+using System;
+
+class Program
+{
+    static void PrintRange(int m, int n)
+   {
+       if (m > n)
+           return;
+        
+        Console.WriteLine(m);
+        PrintRange(m + 1, n);
+    }
+
+    static void Main()
+    {
+       int m = 1;
+       int n = 10;
+       PrintRange(m, n);
+   }
+}
---
+++ hw_2.cs
@@ -0,0 +1,26 @@
+﻿//Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+
+using System;
+
+class Program
+{
+    static int Ackermann(int m, int n)
+{
+if (m == 0)
+            return n + 1;
+        else if (n == 0)
+           return Ackermann(m - 1, 1);
+        else
+           return Ackermann(m - 1, Ackermann(m, n - 1));
+   }
+
+    static void Main()
+    {
+int m = 2;
+int n = 3;
+
+        result = Ackermann(m, n);
+       Console.WriteLine("Результат функции Аккермана для m={0} и n={1}: {2}", m, n, result);
+   }
+}
